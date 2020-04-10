23d861ec5d6ed22cb3ee809af648d7f93ac3af40
Rodrigo Gaona <rodrigogaonagonzalez@gmail.com>
2020-04-10 13:03:12 -0400

    Created new views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+    def hello
+        @message = "Hello from the controller!"
+        @items = ['item1', 'item2', 'item3']
+        render
+    end
+
+    def square
+        @value = params[:value].to_i
+        @result = @value * @value
+        render
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'welcome', to: 'welcome#index'
+  get 'welcome/hello', to: 'welcome#hello'
+  get 'welcome/square', to: 'welcome#square', defaults: { value: 10 }
+  get 'welcome/square/:value', to: 'welcome#square', constraints: { value: /\d{1,10}/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
