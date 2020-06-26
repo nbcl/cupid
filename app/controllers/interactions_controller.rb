04867247ec6cfe3cfd5729722005d6aeaf8d7a07
Rodrigo Gaona Gonzalez <rodrigogaonagonzalez@gmail.com>
2020-06-26 18:07:50 -0400

    merge conflict fixed

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,17 +1,10 @@
 # Class in charge of creating new Interactions
 class InteractionsController < ApplicationController
-<<<<<<< Updated upstream
-    def new
-        @interaction = Interaction.new
-        @destino = User.find(params[:id])
-        @spotify = Admin.find(1)
-    end
-=======
   def new
     @interaction = Interaction.new
     @destino = User.find(params[:id])
+    @spotify = Admin.find(1)
   end
->>>>>>> Stashed changes
 
   def create
     interaction_params = params.require(:citum).permit(:like, :user_id, :user_id_destiny)
